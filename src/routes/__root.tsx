0b7ae2f4f4be993b6d68849753a997dde5f9c465
Steidl, Tim (SE-A/34) <tim.steidl@volkswagen.de>
2024-03-12 11:04:14 +0100

    Added Theme toggle, routing and basic root layout

---
+++ src/routes/__root.tsx
@@ -0,0 +1,42 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import { PlusCircle } from "lucide-react";
+import { Card } from "@/components/ui/card.tsx";
+import { ModeToggle } from "@/components/mode-toggle.tsx";
+import { ThemeProvider } from "@/components/theme-provider.tsx";
+import { Button } from "@/components/ui/button.tsx";
+import { Input } from "@/components/ui/input.tsx";
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <ThemeProvider>
+        <div className="p-2 px-16 flex justify-between bg-primary text-white">
+          <Link to="/" className="text-2xl">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Volkswagen_Logo_till_1995.svg/214px-Volkswagen_Logo_till_1995.svg.png"
+              alt="Volkswagen"
+              className="h-8 w-8 mr-2 inline"
+            />
+          </Link>
+          <Input placeholder={"Start typing..."} autoFocus className={"w-64"} />
+          <div className={"align-center"}>
+            <Button size={"icon"} variant={"ghost"}>
+              <Link to="/create">
+                <PlusCircle />
+              </Link>
+            </Button>
+            <ModeToggle />
+          </div>
+        </div>
+        <hr />
+        <div className={"px-36 pt-4"}>
+          <Card className={"rounded-none h-[calc(100vh-100px)] overflow-auto"}>
+            <Outlet />
+          </Card>
+        </div>
+        <TanStackRouterDevtools />
+      </ThemeProvider>
+    </>
+  ),
+});
